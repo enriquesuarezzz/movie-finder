5d884b314a1c148add73ee3a66506173e6537ad7
enriquesuarezzz <enriquesuarezmartin@gmail.com>
2024-08-21 08:05:35 +0100

    SearchBarUpdate + MInorFixes

--- client/src/components/molecules/navbar/navbar.tsx
+++ client/src/components/molecules/navbar/navbar.tsx
@@ -1,32 +1,37 @@
-import { useState, useEffect } from 'react'
+import React, { useState, useCallback } from 'react'
+import debounce from 'lodash/debounce'
 import Burguer from '../../atoms/svg/burguer'
 import { Logo } from '../../atoms/svg/logo'
 import Search from '../../atoms/svg/search'
-import CloseIcon from '../../atoms/svg/close' // Assuming you have a CloseIcon component
+import CloseIcon from '../../atoms/svg/close'
 
 export function Navbar() {
+  const [searchQuery, setSearchQuery] = useState('')
   const [isMenuOpen, setIsMenuOpen] = useState(false)
+  const [isSearchOpen, setIsSearchOpen] = useState(false)
 
-  useEffect(() => {
-    // Function to check screen size and close the menu if the screen is resized to a large size
-    const handleResize = () => {
-      if (window.innerWidth >= 768) {
-        // Adjust according to your breakpoint
-        setIsMenuOpen(false)
-      }
-    }
+  // Debounced search function
+  const debouncedSearch = useCallback(
+    debounce((query: string) => {
+      console.log('Performing search for:', query)
+      // API when be placed here when it's ready
+    }, 300), // 300ms delay
+    [],
+  )
 
-    // Add event listener to handle screen resize
-    window.addEventListener('resize', handleResize)
-
-    // Cleanup the event listener on component unmount
-    return () => {
-      window.removeEventListener('resize', handleResize)
-    }
-  }, [])
+  // Handle input change and call debounced search
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value
+    setSearchQuery(value)
+    debouncedSearch(value)
+  }
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen)
+  }
+
+  const toggleSearch = () => {
+    setIsSearchOpen(!isSearchOpen)
   }
 
   return (
@@ -39,9 +44,11 @@ export function Navbar() {
         </button>
       </div>
       <div className="flex items-center gap-2 pr-2 font-bold md:gap-8 md:pr-10">
-        <button>
-          <Search />
-        </button>
+        {!isSearchOpen && (
+          <button onClick={toggleSearch}>
+            <Search />
+          </button>
+        )}
         <div className="hidden items-center gap-8 md:flex">
           <button className="hover:text-blue-500">Sign In</button>
           <button className="text-bold rounded-3xl bg-blue-700 px-4 py-2 text-white hover:bg-blue-600">
@@ -55,6 +62,25 @@ export function Navbar() {
         </div>
       </div>
 
+      {/* Search Bar */}
+      {isSearchOpen && (
+        <div className="absolute left-0 right-0 top-full z-50 flex items-center justify-center bg-gray-800 shadow-lg">
+          <button onClick={toggleSearch} className="ml-4 text-white">
+            <Search />
+          </button>
+          <input
+            type="text"
+            className="w-full bg-gray-800 p-2 py-4 text-white focus:outline-none"
+            placeholder="Search for movies..."
+            value={searchQuery}
+            onChange={handleInputChange}
+          />
+          <button onClick={toggleSearch} className="mr-4 text-white">
+            <CloseIcon />
+          </button>
+        </div>
+      )}
+
       {/* Blurred Background Overlay */}
       {isMenuOpen && (
         <div
@@ -65,25 +91,24 @@ export function Navbar() {
 
       {/* Mobile Menu */}
       {isMenuOpen && (
-        <div className="fixed right-0 top-0 z-50 flex h-full w-80 flex-col bg-blue-500/60  p-6 px-10 shadow-lg md:hidden">
+        <div className="fixed right-0 top-0 z-50 flex h-full w-80 flex-col bg-blue-500/60 p-6 px-10 shadow-lg md:hidden">
           <div className="mb-4 flex w-full justify-between">
-            <button className="flex gap-2 text-left text-2xl font-bold text-white ">
+            <button className="flex gap-2 text-left text-2xl font-bold text-white">
               <Logo color="white" />
               MovieFinder
             </button>
             <button onClick={toggleMenu}>
-              <CloseIcon /> {/* This is your close icon */}
+              <CloseIcon />
             </button>
           </div>
           <button className="mb-2 w-full text-left text-lg text-white">
-            {' '}
             My Movies
           </button>
 
-          <button className="mb-2 w-full text-left text-lg text-white ">
+          <button className="mb-2 w-full text-left text-lg text-white">
             Sign In
           </button>
-          <button className="mb-2 w-full text-left text-lg text-white ">
+          <button className="mb-2 w-full text-left text-lg text-white">
             Sign Up now
           </button>
         </div>
